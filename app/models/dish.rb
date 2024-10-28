ad102429afa30a26da814d4028d1ec9ef2840bff
João Victor Castelo Branco Gomes de Medeiros <joao.castelo.medeiros@gmail.com>
2024-10-28 07:25:39 -0300

    add validation to dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,5 @@
 class Dish < ApplicationRecord
   belongs_to :restaurant
+
+  validates :name, :description, :restaurant, presence: true
 end
--- spec/system/menuItems/user_set_menu_items_spec.rb
+++ spec/system/menuItems/user_set_menu_items_spec.rb
@@ -38,4 +38,42 @@ describe 'Usuário cadastra um prato' do
     expect(page).to have_content('Prato Criado com sucesso!')
     expect(page).to have_content('Prato Teste')
   end
+
+  it 'e não consegue fazer o cadastro' do
+    # Arrange
+    user = User.create!(
+      cpf: "109.789.030-99",
+      email:  "sergio.vieira.de.melo@ri.com",
+      name: "Sergio",
+      last_name: "Vieira",
+      password: "nacoesunidas",
+    )
+
+    restaurant = Restaurant.create!(
+      user: user,
+      registered_name: "Arvo",
+      comercial_name: "Arvo Restaurante",
+      cnpj: "61.236.299/0001-72",
+      address: "Av. 1000",
+      phone: "6731423872",
+      email: "arvo@restaurante.com"
+    )
+
+    # Act
+    login_as(user, :scope => :user)
+    visit root_path
+    within('nav') do
+      click_on "Ver Restaurante"
+    end
+    click_on "Adicionar um prato"
+    fill_in 'Nome', with: 'Prato Teste'
+    
+    fill_in 'Calorias', with: 300
+    click_on 'Cadastrar Prato'
+  
+    # Assert
+
+    expect(page).not_to have_content('Prato Criado com sucesso!')
+    expect(page).not_to have_content('Prato Teste')
+  end
 end
