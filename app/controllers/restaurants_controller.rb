8c2ceb38fcbaf7312e6eb16da92a49808fe3df43
João Victor Castelo Branco Gomes de Medeiros <joao.castelo.medeiros@gmail.com>
2024-10-27 23:06:18 -0300

    add form and create method to restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,5 +5,28 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    restaurant_params = params
+    .require(:restaurant)
+    .permit(
+      :registered_name,
+      :comercial_name,
+      :cnpj,
+      :address,
+      :phone,
+      :email
+    )
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    if @restaurant.save!
+      redirect_to @restaurant, notice: "Restaurante cadastrado!"
+    else
+      flash.now[:notice] = "Não foi possível registrar o restaurante"
+      render :new
+    end
   end 
+
+  def show
+    params_id = params[:id]
+    @restaurant = Restaurant.find(params_id)
+  end
 end
